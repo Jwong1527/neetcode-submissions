af12a423913fb76dd2e7685fd8a328203ecea863
Jeffrey Wong <112716191+Jwong1527@users.noreply.github.com>
2026-08-23 15:07:33 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        # Compute n - 1 values
+        # When the 1 variable is at the 0 index we can break out of the loop
+
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            temp = one
+
+            one = one + two
+
+            two = temp # shift it to the previous value of what one was but we need a temp var.
+        
+        return one 
